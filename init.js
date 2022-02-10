c7d0779d0000c958ebc952a3f9f84fd782017ce2
divvya007 <divvyatripaathi@gmail.com>
2022-02-10 16:27:38 +0000

    nothing to show

--- init.js
+++ init.js
@@ -5,35 +5,30 @@ class CoffeeMaker {
     this.coffeePodBox = coffeePodBox;
   }
   dispense(coffeeType) {
-    // if (coffeeType === "milk") {
-    //   this.milkTank.getMilk(250);
-    //   mug.innerHTML = "here is your Coffee";
-    //   contentLeft.innerText = `${this.milkTank.milkQuantity} ml of milk left`;
-    // }
+    if (coffeeType === "milk") {
+      this.milkTank.getMilk(250);
+      mug.innerHTML = "here is your Coffee";
+      contentLeft.innerText = `${this.waterTank.waterQuantity} ml of water left, ${this.milkTank.milkQuantity} ml of milk left, ${this.coffeePodBox.numberOfPods}`;
+      alert("low milk press refill");
+    }
 
     if (coffeeType === "water") {
       this.waterTank.getWater(250);
-      mug.innerHTML = "enjoy your coffee";
-      contentLeft.innerText = `${this.waterTank.waterQuantity} ml of water left`;
+      if (this.waterTank.waterQuantity < 250) {
+        mug.innerHTML = "low water press refill";
+        contentLeft.innerText = `${this.waterTank.waterQuantity} ml of water left, ${this.milkTank.milkQuantity} ml of milk left, ${this.coffeePodBox.numberOfPods}`;
+        alert("low on water press refill");
+      } else {
+        mug.innerHTML = "enjoy your coffee";
+        contentLeft.innerText = `${this.waterTank.waterQuantity} ml of water left, ${this.milkTank.milkQuantity} ml of milk left, ${this.coffeePodBox.numberOfPods}`;
+      }
     }
   }
-  refillMilk(milkTank) {
-    // remainingPods = remainingPods - 1;
-    if (milkTank === 250) {
-      alert("refill your milk container");
-      //   mug.style.display = "none";
-      //   let machine = document.getElementById("machine");
-      //   let refillImg = document.createElement("img");
-      //   refillImg.src = "./refill.gif";
-      //   machine.innerHTML = null;
-      //   machine.append(refillImg);
-    } else {
-    }
-  }
-  refillWater(waterTank) {
-    if (waterTank === 250) {
-      alert("refill your water container");
-    }
+
+  refillMilk(milkTank) {}
+  refillWater() {
+    console.log(this.waterTank);
+    return this.waterTank.refill(1000);
   }
   refillCoffeePods() {
     return;
@@ -59,12 +54,12 @@ class WaterTank {
   constructor(waterQuantity) {
     this.waterQuantity = waterQuantity;
   }
-  refill() {
-    return;
+  refill(qty) {
+    return (this.waterQuantity = qty);
   }
   getWater(qty) {
-    if (this.waterQuantity <= 250) {
-      alert("refill your water");
+    if (this.waterQuantity < 250) {
+      return this.waterQuantity;
     } else {
       this.waterQuantity = this.waterQuantity - qty;
       return this.waterQuantity;
@@ -80,7 +75,6 @@ class CoffeePodBox {
     return;
   }
 }
-// console.log(coffeeMc);
 
 let milkContainer = new MilkTank(1000);
 
@@ -96,7 +90,7 @@ let contentLeft = document.getElementById("contentLeft");
 let dispenseBtn = document
   .getElementById("dispenseBtn")
   .addEventListener("click", handleOnDispenseCoffeeBtnClick);
-// debugger;
+
 function handleOnDispenseCoffeeBtnClick() {
   coffeeMc.dispense("milk");
   console.log(coffeeMc);
@@ -107,4 +101,12 @@ let blackCoffee = document
   .addEventListener("click", handleOnBlackCoffeeBtn);
 function handleOnBlackCoffeeBtn() {
   coffeeMc.dispense("water");
+}
+
+let refillBtn = document
+  .getElementById("refillBtn")
+  .addEventListener("click", handleClickOnRefillBtn);
+
+function handleClickOnRefillBtn() {
+  coffeeMc.refillWater();
 }
